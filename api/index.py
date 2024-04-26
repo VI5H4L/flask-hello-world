f220c86ed0c687cf277f64d9df9861030cd61347
Vishal Kumar <21ucc116@lnmiit.ac.in>
2024-04-26 09:17:04 +0530

    /json route added

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -9,3 +9,15 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/json')
+def json_data():
+    dummy_data = {
+        "name": "John Doe",
+        "age": 30,
+        "email": "john@example.com"
+    }
+    return jsonify(dummy_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
