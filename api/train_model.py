df331ad958264f02e6482867f34ca88863196a4b
Vishal Kumar <21ucc116@lnmiit.ac.in>
2024-04-27 19:33:06 +0530

    updates

--- api/train_model.py
+++ api/train_model.py
@@ -1,15 +1,15 @@
-# from sklearn.datasets import load_iris
-# from sklearn.model_selection import train_test_split
-# from sklearn.tree import DecisionTreeClassifier
-# import joblib
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+import joblib
 
-# iris = load_iris()
-# X = iris.data
-# y = iris.target
+iris = load_iris()
+X = iris.data
+y = iris.target
 
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# model = DecisionTreeClassifier()
-# model.fit(X_train, y_train)
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
 
-# joblib.dump(model, 'iris_model.pkl')
+joblib.dump(model, 'iris_model.pkl')
