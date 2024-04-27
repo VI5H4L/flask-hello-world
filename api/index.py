fe65e917927c7d8c8270811deca0da31f29398f2
Vishal Kumar <21ucc116@lnmiit.ac.in>
2024-04-27 09:29:06 +0530

    Model training and using

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import joblib
 
 app = Flask(__name__)
+
+model = joblib.load('iris_model.pkl')
 
 @app.route('/')
 def home():
@@ -18,6 +21,13 @@ def json_data():
         "email": "john@example.com"
     }
     return jsonify(dummy_data)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    prediction = model.predict([[data['sepal_length'], data['sepal_width'], data['petal_length'], data['petal_width']]])
+    return jsonify({'prediction': prediction.tolist()})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
